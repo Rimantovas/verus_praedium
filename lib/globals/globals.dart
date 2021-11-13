45e7e7efb87a5680cd5d6d3a995e7e6eea4cc02f
Rimantovas <rimas.pov@gmail.com>
2021-11-13 16:20:03 +0200

    a lot

--- lib/globals/globals.dart
+++ lib/globals/globals.dart
@@ -1,23 +1,114 @@
-List<Map<String, String>> images = [
+List<Map<String, dynamic>> images = [
   {
-    'Kaunas':
+    'name': 'Kaunas',
+    'enum': Miestai.kaunas,
+    'url':
         'https://c4.wallpaperflare.com/wallpaper/877/260/932/the-city-cathedral-lithuania-kaunas-wallpaper-preview.jpg'
   },
-  {'Vilnius': 'https://wallpapercave.com/wp/wp4187171.jpg'},
   {
-    'Klaipeda':
+    'name': 'Vilnius',
+    'enum': Miestai.vilnius,
+    'url': 'https://wallpapercave.com/wp/wp4187171.jpg'
+  },
+  {
+    'name': 'Klaipėda',
+    'enum': Miestai.klaipeda,
+    'url':
         'https://media.gettyimages.com/photos/high-angle-view-of-cityscape-against-sky-picture-id892905584?k=20&m=892905584&s=612x612&w=0&h=zIAvh3RP5J_HdCjplniGm-R-QhJYCgPSqvp7lsazQ7Y='
   },
   {
-    'Siauliai':
+    'name': 'Šiauliai',
+    'enum': Miestai.siauliai,
+    'url':
         'https://image.shutterstock.com/image-photo/siauliai-city-scapes-600w-780934642.jpg'
   },
   {
-    'Panevezys':
+    'name': 'Panevėžys',
+    'enum': Miestai.panevezys,
+    'url':
         'https://image.shutterstock.com/image-photo/aerial-view-panevezys-cathedral-260nw-1399228874.jpg'
   },
   {
-    'Alytus':
+    'name': 'Alytus',
+    'enum': Miestai.alytus,
+    'url':
         'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Alytus.png/375px-Alytus.png'
   }
 ];
+
+List<Map<String, dynamic>> sliderInfo = [
+  {'name': Rodikliai.nusikaltimai, 'value': .5, 'check': true},
+  {'name': Rodikliai.pastatuSkaicius, 'value': .5, 'check': true},
+  {'name': Rodikliai.namuKainos, 'value': .5, 'check': true},
+  {'name': Rodikliai.butuKainos, 'value': .5, 'check': true},
+  {'name': Rodikliai.uzimtumas, 'value': .5, 'check': true},
+  {'name': Rodikliai.atlyginimas, 'value': .5, 'check': true},
+  {'name': Rodikliai.darzeliai, 'value': .5, 'check': true},
+  {'name': Rodikliai.mokyklos, 'value': .5, 'check': true}
+];
+
+enum Rodikliai {
+  nusikaltimai,
+  pastatuSkaicius,
+  namuKainos,
+  butuKainos,
+  uzimtumas,
+  atlyginimas,
+  darzeliai,
+  mokyklos,
+}
+
+extension RodikliuPavadinimai on Rodikliai {
+  String get name {
+    switch (this) {
+      case Rodikliai.nusikaltimai:
+        return 'Nusikaltimų skaičius:';
+      case Rodikliai.pastatuSkaicius:
+        return 'Baigtų statyti pastatų skaičius:';
+      case Rodikliai.namuKainos:
+        return 'Namų vidutinės kainos:';
+      case Rodikliai.butuKainos:
+        return 'Butų vidutinės kainos:';
+      case Rodikliai.uzimtumas:
+        return 'Užimtumo lygis:';
+      case Rodikliai.atlyginimas:
+        return 'Darbo užmokestis:';
+      case Rodikliai.darzeliai:
+        return 'Darželiu skaičius:';
+      case Rodikliai.mokyklos:
+        return 'Mokyklų skaičius:';
+      default:
+        return '';
+    }
+  }
+}
+
+enum Miestai {
+  kaunas,
+  vilnius,
+  alytus,
+  panevezys,
+  klaipeda,
+  siauliai,
+}
+
+extension MiestuPavadinimai on Miestai {
+  String get name {
+    switch (this) {
+      case Miestai.kaunas:
+        return 'Kauno apskritis';
+      case Miestai.vilnius:
+        return 'Vilniaus apskritis';
+      case Miestai.alytus:
+        return 'Alytaus apskritis';
+      case Miestai.panevezys:
+        return 'Panevėžio apskritis';
+      case Miestai.klaipeda:
+        return 'Klaipėdos apskritis';
+      case Miestai.siauliai:
+        return 'Šiaulių apskritis';
+      default:
+        return '';
+    }
+  }
+}
--- lib/pages/cities_page.dart
+++ lib/pages/cities_page.dart
@@ -1,27 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:verus_praedium/globals/globals.dart';
+import 'package:verus_praedium/models/city.dart';
+import 'package:verus_praedium/pages/map.dart';
 import 'package:verus_praedium/pages/statistics.dart';
 import 'package:verus_praedium/widgets/city_card.dart';
 
 class CitiesPage extends StatelessWidget {
-  const CitiesPage({Key? key}) : super(key: key);
-
+  const CitiesPage({Key? key, required this.cities}) : super(key: key);
+  final List<City> cities;
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            ),
+            onPressed: () =>
+                Navigator.canPop(context) ? Navigator.pop(context) : null,
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.map,
+                color: Colors.white,
+              ),
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => MapPage(
+                            cities: cities,
+                          ))),
+            ),
+          ]),
+      body: Center(
         child: Wrap(
           children: [
-            ...images.map(
+            ...cities.map(
               (e) => Padding(
                 padding: const EdgeInsets.all(15),
                 child: CityCard(
-                  city: e.keys.first,
-                  imageUrl: e.values.first,
+                  city: e,
                   onTap: () => Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const StatisticsPage())),
+                          builder: (context) => StatisticsPage(city: e))),
                 ),
               ),
             ),
